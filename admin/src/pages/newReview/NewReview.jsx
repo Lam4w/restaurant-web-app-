cbd336d3408404d36a6d55984ee31b965769baec
Your Name <you@example.com>
2023-04-13 21:44:29 +0700

    kien update

--- admin/src/pages/newReview/NewReview.jsx
+++ admin/src/pages/newReview/NewReview.jsx
@@ -1,24 +1,80 @@
 import "./newReview.css";
+import { useState } from "react";
+import storage from '../../firebase';
+import { useNavigate } from "react-router-dom";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const navigate = useNavigate();
+  const [review, setReview] = useState({});
+  const [img, setImg] = useState({});
+  const [uploaded, setUploaded] = useState(false);
+  const token = useSelector((state) => state.token);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setReview({...review, [e.target.name]: value});
+    console.log(review);
+  }
+
+  const upload = (item) => {
+    const fileName = new Date().getTime() + item.file.name;
+    const storageRef = ref(storage ,`/items/${fileName}`);
+    const uploadTask = uploadBytesResumable(storageRef, item.file);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = Math.round(
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      );
+
+      console.log('upload is' + progress + '% done');
+    },
+    (err) => console.log(err),
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+        setReview((prev) => {
+          return { ...prev, "picturePath": url };
+        });
+        setUploaded(true);
+      })
+    });
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    upload({file: img})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post("http://localhost:3001/reviews/", review, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    navigate('/reviews');
+  }
 
 export default function NewReview() {
   return (
     <div className="newReview">
       <h1 className="newReviewTitle">New Review</h1>
       <form className="newReviewForm">
-        <div className="newReviewItem">
+        {/* <div className="newReviewItem">
           <label>Name of Reviewer</label>
           <input type="text" placeholder="name" />
-        </div>
+        </div> */}
         <div className="newReviewItem">
-          <label>Name of Food</label>
-          <input type="text" placeholder="food" />
+          <label>Name of review</label>
+          <input type="text" placeholder="review" onChange={handleChange}/>
         </div>
         <div className="newReviewItem">
           <label>Describe</label>
-          <input type="email" placeholder="describe" />
+          <input type="email" placeholder="describe" onChange={handleChange}/>
         </div>
         
-        <button className="newReviewButton">Create</button>
+        <button type="button" className="newReviewButton" onClick={handleSubmit}>Create</button>
       </form>
     </div>
   );
--- admin/src/pages/reviewList/ReviewList.jsx
+++ admin/src/pages/reviewList/ReviewList.jsx
@@ -71,68 +71,94 @@
 //   );
 // }
 
-// import "./reviewList.css";
-// import { DataGrid } from "@material-ui/data-grid";
-// import { DeleteOutline } from "@material-ui/icons";
-// import { reviewRows } from "../../dummyData";
-// import { Link } from "react-router-dom";
-// import { useState } from "react";
-// import {results} from "../../pages/reviewList/reviewList";
+import "./reviewList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { reviewRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from 'react-redux';
 
-// export default function ReviewList() {
-//   const [data, setData] = useState(reviewRows);
 
-//   const handleDelete = (id) => {
-//     setData(data.filter((item) => item.id !== id));
-//   };
-//   const [data, setData] = useState(results);
+export default function ReviewList() {
+  const [data, setData] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
+  let dataIds = [];
+  const token = useSelector((state) => state.token);
 
-// //   const handleDelete = (id) => {
-// //     setData(data.filter((item) => item.id !== id));
-// //   };
+  const getReviews = async () => {
+    const response = await axios.get('http://localhost:3001/reviews');
+
+    const resData = response.data;
+    setData(resData);
+  }
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/reviews/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    setData(data.filter((item) => item._id !== id));
+  };
+
+  const handleDeleteAll = async () => {
+    if (dataIds.length > 1) {
+      const idList = dataIds.join('_'); 
+      await axios.delete(`http://localhost:3001/Reviews/many/${idList}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(dataIds);
+      console.log(dataIds.length)
   
-//   const columns = [
-//     { field: "id", headerName: "ID", width: 90 },
-//     // { field: "reviewer", headerName: "Reviews", width: 150 },
+      setData(data.filter((item) => !dataIds.includes(item._id)));
+    }
+  }
+
+  useEffect(() => {
+    getReviews()
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    // { field: "reviewer", headerName: "Reviews", width: 150 },
 
     
-//     { field: "name", headerName: "Name", width: 200 },
-//     {
-//       field: "des",
-//       headerName: "Describe",
-//       width: 521,
-//     },
+    { field: "name", headerName: "Name", width: 200 },
+    {
+      field: "des",
+      headerName: "Describe",
+      width: 521,
+    },
    
-//     {
-//       field: "action",
-//       headerName: "Action",
-//       width: 150,
-//       renderCell: (params) => {
-//         return (
-//           <>
-//             <Link to={"/review/" + params.row.id}>
-//               <button className="reviewListEdit">Edit</button>
-//             </Link>
-//             <DeleteOutline
-//               className="reviewListDelete"
-//               onClick={() => handleDelete(params.row.id)}
-//             />
-//           </>
-//         );
-//       },
-//     },
-//   ];
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            {/* <Link to={"/review/" + params.row.id}>
+              <button className="reviewListEdit">Edit</button>
+            </Link> */}
+            <DeleteOutline
+              className="reviewListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
 
-//   return (
-//     <div className="reviewList">
-//       <DataGrid
-//         rows={data}
-//         disableSelectionOnClick
-//         columns={columns}
-//         pageSize={8}
-//         checkboxSelection
-//       />
-//     </div>
-//   );
-// }
+  return (
+    <div className="reviewList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+  );
+}
 
